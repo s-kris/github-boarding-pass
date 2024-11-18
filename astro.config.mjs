c0ae40cb0e9ea73b6b5428bada5b9be7b7710fc2
Sai Krishna <skris4291@gmail.com>
2024-11-19 02:13:24 +0530

    chore!: change deploy target to node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server'
+  output: 'server',
+  adapter: node({
+    mode: 'standalone' // 'standalone' is best for production deployments
+  })
 });
